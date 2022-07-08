b898b7271d7208d61df3c829ab145349bd13631b
Akshayalekshmi <akshayamakairam@gmail.com>
2022-07-08 11:59:23 +0530

    Added

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -11,6 +11,13 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  name="Akku"
+  data=[
+  {"name":"Rahul","age":45,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+  {"name":"Akku","age":19,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+  {"name":"Miya","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+  {"name":"Appu","age":28,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+  {"name":"Mano","age":29,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+  {"name":"Arya","age":18,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumvkZyOJn7TLV3Jt_j-Cdppg4uMrfpOtQtcVgTveuagRqFC4XkNe-hNq3xaNvjk34ksA&usqp=CAU"},
+]
 
 }
